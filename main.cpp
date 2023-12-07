f49b65f6dfa3fc0317a8173ce3210722ee389741
John R. O'Donnell <matadorcrab@gmail.com>
2023-12-07 15:48:04 -0600

    init

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+  int favorite_number;
+  std::cout << "Enter your favorite number between 1 and 100";
+
+  std::cin >> favorite_number;
+
+  std::cout << "Good Number!" << std::endl;
+  return 0;
+}
